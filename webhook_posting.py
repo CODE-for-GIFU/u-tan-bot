5665ff5d20f31f5888e4545bddee45895fd2184c
Ryo Takahashi <r-takahashi@eyetech.jp>
2021-05-30 22:01:52 +0900

    pysen format

--- webhook_posting.py
+++ webhook_posting.py
@@ -1,9 +1,10 @@
-from enum import Enum
 import json
 import os
-import requests
-import time
 import random
+import time
+from enum import Enum
+
+import requests
 
 try:
     from dotenv import load_dotenv
@@ -11,6 +12,7 @@ try:
     load_dotenv()
 except ImportError:
     print(__file__ + r":環境変数の読込にdotenvを使用せず続行します。")
+
 
 class WebhookPosting:
     class ScriptKeys(Enum):
@@ -28,13 +30,13 @@ class WebhookPosting:
     __URUN_WEBHOOK = os.getenv(r"URUN_INCOMING_WEBHOOK")
     __UTARO_WEBHOOK = os.getenv(r"UTARO_INCOMING_WEBHOOK")
 
-    def __get_umember_type(self, member_name: str)->UMember:
+    def __get_umember_type(self, member_name: str) -> UMember:
         assert isinstance(member_name, str)
 
         if self.UMember.Utan.value == member_name:
             return self.UMember.Utan
         elif self.UMember.Utako.value == member_name:
-            return self.UMember.Utako 
+            return self.UMember.Utako
         elif self.UMember.Urun.value == member_name:
             return self.UMember.Urun
         elif self.UMember.Utako.value == member_name:
@@ -55,7 +57,7 @@ class WebhookPosting:
             AssertionError(r"Undefined UMember Type" + member)
 
     def __call__(self, script: list):
-        assert isinstance(script , list)
+        assert isinstance(script, list)
 
         for item in script:
             assert isinstance(item, dict)
@@ -65,16 +67,9 @@ class WebhookPosting:
 
             webhook = self.__get_webhook_url(self.__get_umember_type(umember))
 
-            self.__post_incoming_webhook(url = webhook, comment = comment)
+            self.__post_incoming_webhook(url=webhook, comment=comment)
 
             time.sleep(1 + random.randint(0, 5) * 0.1)
-        
-    def __post_incoming_webhook(self, url: str, comment:str):
-        requests.post(
-            url = url,
-            data = json.dumps(
-                {
-                    r"text": comment
-                }
-            )
-        )
+
+    def __post_incoming_webhook(self, url: str, comment: str):
+        requests.post(url=url, data=json.dumps({r"text": comment}))
